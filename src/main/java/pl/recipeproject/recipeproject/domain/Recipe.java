47d24a059a6bc7425be315686a5941423175ff70
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-13 13:11:47 +0100

    @ManytoMany - categories

--- src/main/java/pl/recipeproject/recipeproject/domain/Recipe.java
+++ src/main/java/pl/recipeproject/recipeproject/domain/Recipe.java
@@ -9,7 +9,7 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String desription;
+    private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
@@ -24,7 +24,11 @@ public class Recipe {
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients = new HashSet<>();
-
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -34,12 +38,12 @@ public class Recipe {
         this.id = id;
     }
 
-    public String getDesription() {
-        return desription;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDesription(String desription) {
-        this.desription = desription;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Integer getPrepTime() {
@@ -90,6 +94,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Difficulity getDifficulity() {
+        return difficulity;
+    }
+
+    public void setDifficulity(Difficulity difficulity) {
+        this.difficulity = difficulity;
+    }
+
     public Byte[] getImage() {
         return image;
     }
@@ -106,19 +118,19 @@ public class Recipe {
         this.notes = notes;
     }
 
-    public Difficulity getDifficulity() {
-        return difficulity;
-    }
-
-    public void setDifficulity(Difficulity difficulity) {
-        this.difficulity = difficulity;
-    }
-
     public Set<Ingredient> getIngredients() {
         return ingredients;
     }
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
