7b95070a602e7565f1fbc9357b37a9ac75418d8c
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-15 20:09:39 +0100

    added Spring test

---
+++ src/test/java/pl/recipeproject/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,32 @@
+package pl.recipeproject.recipeproject.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.recipeproject.recipeproject.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+}
