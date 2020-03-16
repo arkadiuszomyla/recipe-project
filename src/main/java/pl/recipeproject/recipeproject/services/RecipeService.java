11fd8277df5b96482a13bd64c8794666b8c8931a
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-16 17:27:05 +0100

    added @PostMapping

--- src/main/java/pl/recipeproject/recipeproject/services/RecipeService.java
+++ src/main/java/pl/recipeproject/recipeproject/services/RecipeService.java
@@ -1,5 +1,6 @@
 package pl.recipeproject.recipeproject.services;
 
+import pl.recipeproject.recipeproject.commands.RecipeCommand;
 import pl.recipeproject.recipeproject.domain.Recipe;
 
 import java.util.Set;
@@ -7,4 +8,6 @@ import java.util.Set;
 public interface RecipeService {
     Set<Recipe> getRecipes();
     Recipe findById(Long l);
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
 }
